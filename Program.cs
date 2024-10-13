be58099fda8679e2ba04ff9c1d54e2cc5066e0a1
Murdy093 <qwerty2018228@gmail.com>
2024-10-13 14:26:14 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        int[] numbers = new int[10];
+
+        Console.WriteLine("Введіть 10 цілих чисел:");
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write($"Число {i + 1}: ");
+            numbers[i] = int.Parse(Console.ReadLine());
+        }
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = Math.Abs(numbers[i]);
+        }
+
+        Console.WriteLine("Масив після заміни елементів на їх модулі:");
+        foreach (int num in numbers)
+        {
+            Console.Write(num + " ");
+        }
+
+        Console.WriteLine("\nPress any key to complete...");
+        Console.ReadKey();
+
+    }
+}
